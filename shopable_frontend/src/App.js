49623be239ece0ae2862e870dcf611c2f9ba324e
Riddhi2291 <riddhi2291@gmail.com>
2023-06-23 15:22:18 -0400

    Setup frontend app

---
+++ shopable_frontend/src/App.js
@@ -0,0 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
